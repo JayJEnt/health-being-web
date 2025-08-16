a1ceae338fb925b68fe3cc93757f05a9ccb293d6
Mikolaj Mozoluk <mikolaj.moozoluk@gmail.com>
2025-08-16 18:31:41 +0200

    userCard, edytowanie i usuwanie usera

--- src/pages/UsersList.tsx
+++ src/pages/UsersList.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { api } from "../api/api";
 import type { User } from "../types/user";
+import UserCard from "../components/UsersList/UserCard";
 const UsersList: React.FC = () => {
     const [usersList, setUsersList] = useState<User[]>([]);
     useEffect(() => {
@@ -8,16 +9,22 @@ const UsersList: React.FC = () => {
             setUsersList(res);
         });
     }, []);
+    const handleDelete = async (deletedUser: User) => {
+        try {
+            await api.delete(`/users/${deletedUser.id}`);
+            const newUsersList = usersList.filter(
+                (user) => user.id !== deletedUser.id,
+            );
+            setUsersList(newUsersList);
+        } catch (err) {
+            console.log(err);
+        }
+    };
     return (
         <div>
             <div>
                 {usersList.map((user) => (
-                    <div key={user.id}>
-                        <a>{user.username}</a>
-                        <a>{user.email}</a>
-                        <a>{user.id}</a>
-                        <a>{user.role}</a>
-                    </div>
+                    <UserCard key={user.id} user={user} deleteHandler={handleDelete} />
                 ))}
             </div>
         </div>
