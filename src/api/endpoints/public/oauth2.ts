163e1e1c1c6ce2bdbd9aed64a1cbfa3bb28c8bb0
KamilMehring <kamil.mehring@onet.pl>
2025-10-25 16:35:48 +0200

    add verification of password strength

--- src/api/endpoints/public/oauth2.ts
+++ src/api/endpoints/public/oauth2.ts
@@ -5,7 +5,7 @@ import type { User, UserCreate } from '../../models/user';
 
 export const oauth2Api = {
   ourRegister: (user: UserCreate) =>
-    api.get<User>(`${settings.OAUTH2_OUR_REGISTER_ENDPOINT}`, { user }),
+    api.post<User>(`${settings.OAUTH2_OUR_REGISTER_ENDPOINT}`, user),
 
   ourLogin: (credentials: Omit<UserCreate, 'email'>) =>
     api.postForm<Token>(`${settings.OAUTH2_OUR_LOGIN_ENDPOINT}`, credentials),
