340442e144f92e32e8a18a8084d9d82b82addda9
KamilMehring <kamil.mehring@onet.pl>
2025-11-01 22:21:54 +0100

    Feat-add-recipe-search-page

--- src/api/models/recipe.ts
+++ src/api/models/recipe.ts
@@ -24,3 +24,12 @@ export interface RecipeResponse extends Recipe {
   diet?: DietCreate[] | null;
   ingredient: IngredientQuantity[];
 }
+
+export interface RecipeFilter {
+  allergies_off?: boolean;
+  dislike_off?: boolean;
+  only_favourite_ingredients?: boolean;
+  only_favourite_diets?: boolean;
+  only_followed_authors?: boolean;
+  only_owned_ingredients?: boolean;
+}
