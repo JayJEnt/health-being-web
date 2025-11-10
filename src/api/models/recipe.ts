d21ecf8747e634f1d90e5cadc4e42004586c16ae
JayJCode <jayjcode@gmail.com>
2025-11-10 21:48:03 +0100

    Updated models to the newest backend version

--- src/api/models/recipe.ts
+++ src/api/models/recipe.ts
@@ -1,5 +1,7 @@
 import type { DietCreate } from './diet';
 import type { IngredientQuantity } from './ingredient';
+import type { MicronutrientsTotal } from './utils';
+import type { Category } from './enum_utils';
 
 export interface RecipeCreate {
   title: string;
@@ -7,6 +9,7 @@ export interface RecipeCreate {
   instructions: string[];
   diet?: DietCreate[] | null;
   ingredient: IngredientQuantity[];
+  category: Category;
 }
 
 export interface RecipeOverview {
@@ -18,9 +21,11 @@ export interface Recipe extends RecipeOverview {
   author_id: number;
   description: string;
   instructions: string[];
+  category: Category;
 }
 
 export interface RecipeResponse extends Recipe {
   diet?: DietCreate[] | null;
   ingredient: IngredientQuantity[];
+  micronutrients?: MicronutrientsTotal | {};
 }
